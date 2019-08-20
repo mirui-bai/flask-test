2bcc4bd452f67c0c0e37666a881095bd3dea73bd
mirui <aglassboy@163.com>
2019-08-20 16:17:36 +0800

    test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import unittest
+import re
+import time
 import threading
 from app import create_app, db
-from app.models import User, Role, Post, Comment
+from app.models import User, Role, Post
 
 
 class SeleniumTestCase(unittest.TestCase):
@@ -34,15 +36,17 @@ class SeleniumTestCase(unittest.TestCase):
             Post.generate_fake(10)
 
             # 添加管理员
-            admin_role = Role.query.filter_by(permission=0xff).first()
+            admin_role = Role.query.filter_by(permissions=0xff).first()
             admin = User(email='john@example.com',
-                         username='john',
+                         username='john', password='cat',
                          role=admin_role, confirmed=True)
             db.session.add(admin)
             db.session.commit()
 
             # 在一个线程中启动flask服务器
             threading.Thread(target=cls.app.run).start()
+
+            time.sleep(1)
 
     @classmethod
     def tearDownClass(cls):
@@ -65,5 +69,28 @@ class SeleniumTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_admin_home_page(self):
+        # 进入首页
+        self.client.get('http://localhost:5000/')
+        # print(self.client.page_source)
+        self.assertTrue(re.search('Hello,\s+Stranger', self.client.page_source))
+        #  <p>Pleased to meet you!</p>
+
+        # 进入登录页面
+        self.client.find_element_by_link_text('Log In').click()
+        # print(self.client.page_source)
+        self.assertTrue('<h1>Login</h1>' in self.client.page_source)
+        # 登录
+        self.client.find_element_by_name('email').\
+            send_keys('john@example.com')
+        self.client.find_element_by_name('password').send_keys('cat')
+        self.client.find_element_by_name('submit').click()
+        print(self.client.page_source)
+        self.assertTrue(re.search('Hello,\s+john', self.client.page_source))
+
+        # 进入用户个人资料页面
+        self.client.find_element_by_link_text('john').click()
+        # print(self.client.page_source)
+        self.assertTrue('<h1>User</h1>' in self.client.page_source)
 
 
